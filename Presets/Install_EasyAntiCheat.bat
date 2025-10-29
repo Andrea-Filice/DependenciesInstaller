c1cfa5dfa67bb65558c59be6d08f4caab5900124
Play-Epik-Inc <filiceandrea8@gmail.com>
2025-10-29 20:44:35 +0100

    chore: bumped packages
    * bumped easy anti cheat package

--- Presets/Install_EasyAntiCheat.bat
+++ Presets/Install_EasyAntiCheat.bat
@@ -2,7 +2,7 @@
 
 NET SESSION >nul 2>&1
 if %errorLevel% == 0 (
-    "%~dp0\EasyAntiCheat\EasyAntiCheat_EOS_Setup.exe" install bca5888500f422
+    "%~dp0\EasyAntiCheat\EasyAntiCheat_EOS_Setup.exe" install 898a1f5bc39850
 ) else (
     echo This script requires administrator privileges.
     echo Please run it as an administrator.
