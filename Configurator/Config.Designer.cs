9f0077109e44195741c80a1b4109807aee9240fa
Andrea-Filice <filiceandrea8@gmail.com>
2025-11-02 15:28:59 +0100

    feat: added context menu
    * added a menu in the top border with "File", "Info" and "Help" sub-menus

--- Configurator/Config.Designer.cs
+++ Configurator/Config.Designer.cs
@@ -42,6 +42,17 @@ namespace Configurator
             this.progressBar = new System.Windows.Forms.ProgressBar();
             this.executeButton = new System.Windows.Forms.Button();
             this.versionNumber = new System.Windows.Forms.Label();
+            this.menuBar = new System.Windows.Forms.MenuStrip();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.checkForUpdatesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.visitWebsiteToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sendAFeedbackToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bugReportToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuBar.SuspendLayout();
             this.SuspendLayout();
             // 
             // labelStart
@@ -50,7 +61,7 @@ namespace Configurator
             this.labelStart.BackColor = System.Drawing.SystemColors.ButtonFace;
             this.labelStart.Font = new System.Drawing.Font("Clash Display Medium", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.labelStart.ForeColor = System.Drawing.SystemColors.MenuHighlight;
-            this.labelStart.Location = new System.Drawing.Point(46, 9);
+            this.labelStart.Location = new System.Drawing.Point(50, 48);
             this.labelStart.Name = "labelStart";
             this.labelStart.Size = new System.Drawing.Size(703, 38);
             this.labelStart.TabIndex = 0;
@@ -60,7 +71,7 @@ namespace Configurator
             // 
             this.gameFolder.AutoSize = true;
             this.gameFolder.Font = new System.Drawing.Font("Clash Display Medium", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.gameFolder.Location = new System.Drawing.Point(48, 77);
+            this.gameFolder.Location = new System.Drawing.Point(52, 98);
             this.gameFolder.Name = "gameFolder";
             this.gameFolder.Size = new System.Drawing.Size(172, 26);
             this.gameFolder.TabIndex = 1;
@@ -71,7 +82,7 @@ namespace Configurator
             this.buttonChangeFolder.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
             this.buttonChangeFolder.Font = new System.Drawing.Font("Clash Display Medium", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonChangeFolder.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.buttonChangeFolder.Location = new System.Drawing.Point(627, 77);
+            this.buttonChangeFolder.Location = new System.Drawing.Point(631, 98);
             this.buttonChangeFolder.Name = "buttonChangeFolder";
             this.buttonChangeFolder.Size = new System.Drawing.Size(146, 31);
             this.buttonChangeFolder.TabIndex = 2;
@@ -84,7 +95,7 @@ namespace Configurator
             // 
             this.folderSelected.AutoSize = true;
             this.folderSelected.Font = new System.Drawing.Font("Clash Display Medium", 13.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.folderSelected.Location = new System.Drawing.Point(226, 77);
+            this.folderSelected.Location = new System.Drawing.Point(230, 98);
             this.folderSelected.Name = "folderSelected";
             this.folderSelected.Size = new System.Drawing.Size(223, 26);
             this.folderSelected.TabIndex = 3;
@@ -167,8 +178,92 @@ namespace Configurator
             this.versionNumber.Name = "versionNumber";
             this.versionNumber.Size = new System.Drawing.Size(337, 19);
             this.versionNumber.TabIndex = 9;
-            this.versionNumber.Text = "";
+            this.versionNumber.Text = "Dependencies Installer v.";
             this.versionNumber.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // menuBar
+            // 
+            this.menuBar.BackColor = System.Drawing.Color.Transparent;
+            this.menuBar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.menuBar.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuBar.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem1,
+            this.aboutToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuBar.Location = new System.Drawing.Point(0, 0);
+            this.menuBar.Name = "menuBar";
+            this.menuBar.Size = new System.Drawing.Size(800, 28);
+            this.menuBar.TabIndex = 10;
+            this.menuBar.Text = "menu";
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem,
+            this.checkForUpdatesToolStripMenuItem});
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(46, 24);
+            this.toolStripMenuItem1.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.exitToolStripMenuItem.Text = "Check for Updates";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // checkForUpdatesToolStripMenuItem
+            // 
+            this.checkForUpdatesToolStripMenuItem.Name = "checkForUpdatesToolStripMenuItem";
+            this.checkForUpdatesToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.checkForUpdatesToolStripMenuItem.Text = "Exit";
+            this.checkForUpdatesToolStripMenuItem.Click += new System.EventHandler(this.checkForUpdatesToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem1,
+            this.visitWebsiteToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(49, 24);
+            this.aboutToolStripMenuItem.Text = "Info";
+            // 
+            // aboutToolStripMenuItem1
+            // 
+            this.aboutToolStripMenuItem1.Name = "aboutToolStripMenuItem1";
+            this.aboutToolStripMenuItem1.Size = new System.Drawing.Size(224, 26);
+            this.aboutToolStripMenuItem1.Text = "About";
+            this.aboutToolStripMenuItem1.Click += new System.EventHandler(this.aboutToolStripMenuItem1_Click);
+            // 
+            // visitWebsiteToolStripMenuItem
+            // 
+            this.visitWebsiteToolStripMenuItem.Name = "visitWebsiteToolStripMenuItem";
+            this.visitWebsiteToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.visitWebsiteToolStripMenuItem.Text = "Visit website";
+            this.visitWebsiteToolStripMenuItem.Click += new System.EventHandler(this.visitWebsiteToolStripMenuItem_Click);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sendAFeedbackToolStripMenuItem,
+            this.bugReportToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(55, 24);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // sendAFeedbackToolStripMenuItem
+            // 
+            this.sendAFeedbackToolStripMenuItem.Name = "sendAFeedbackToolStripMenuItem";
+            this.sendAFeedbackToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.sendAFeedbackToolStripMenuItem.Text = "Send a Feedback";
+            this.sendAFeedbackToolStripMenuItem.Click += new System.EventHandler(this.sendAFeedbackToolStripMenuItem_Click);
+            // 
+            // bugReportToolStripMenuItem
+            // 
+            this.bugReportToolStripMenuItem.Name = "bugReportToolStripMenuItem";
+            this.bugReportToolStripMenuItem.Size = new System.Drawing.Size(224, 26);
+            this.bugReportToolStripMenuItem.Text = "Bug Report";
+            this.bugReportToolStripMenuItem.Click += new System.EventHandler(this.bugReportToolStripMenuItem_Click);
             // 
             // Config
             // 
@@ -187,11 +282,15 @@ namespace Configurator
             this.Controls.Add(this.buttonChangeFolder);
             this.Controls.Add(this.gameFolder);
             this.Controls.Add(this.labelStart);
+            this.Controls.Add(this.menuBar);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuBar;
             this.Name = "Config";
             this.Text = "Configurator";
             this.Load += new System.EventHandler(this.Config_Load);
+            this.menuBar.ResumeLayout(false);
+            this.menuBar.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -243,5 +342,15 @@ namespace Configurator
         private Button executeButton;
         internal Button buildButton;
         private Label versionNumber;
+        private MenuStrip menuBar;
+        private ToolStripMenuItem toolStripMenuItem1;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private ToolStripMenuItem checkForUpdatesToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem;
+        private ToolStripMenuItem aboutToolStripMenuItem1;
+        private ToolStripMenuItem visitWebsiteToolStripMenuItem;
+        private ToolStripMenuItem helpToolStripMenuItem;
+        private ToolStripMenuItem sendAFeedbackToolStripMenuItem;
+        private ToolStripMenuItem bugReportToolStripMenuItem;
     }
 }
--- Configurator/Config.cs
+++ Configurator/Config.cs
@@ -2,11 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using Newtonsoft.Json.Linq;
 
 namespace Configurator
 {
@@ -17,9 +21,64 @@ namespace Configurator
             this.MaximizeBox = false;
             this.MinimizeBox = true;
         }
+
         private void Config_Load(object sender, EventArgs e) {Console.WriteLine($"{DateTime.Now}: Loading config files");}
         private void buttonChangeFolder_Click(object sender, EventArgs e) {OpenFolderBrowser();}
         private void buildButton_Click(object sender, EventArgs e) {StartBuilding();}
         private void executeButton_Click(object sender, EventArgs e) {ExecuteProgram();}
+
+
+        //NOTE: MENU ACTIONS
+        private void checkForUpdatesToolStripMenuItem_Click(object sender, EventArgs e){Application.Exit();}
+
+        private void visitWebsiteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo website = new ProcessStartInfo("https://play-epik-incorporation.netlify.app/developers#dependenciesInstaller");
+            Process.Start(website);
+        }
+
+        private void aboutToolStripMenuItem1_Click(object sender, EventArgs e){MessageBox.Show("Dependencies Installer (v. " + Application.ProductVersion + ").", "About Dependencies Installer", MessageBoxButtons.OK);}
+
+        private void sendAFeedbackToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo website = new ProcessStartInfo("https://github.com/Andrea-Filice/DependenciesInstaller/issues/new?labels=question");
+            Process.Start(website);
+        }
+
+        private void bugReportToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ProcessStartInfo website = new ProcessStartInfo("https://github.com/Andrea-Filice/DependenciesInstaller/issues/new?labels=bug");
+            Process.Start(website);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (WebClient wc = new WebClient())
+            {
+                var json = wc.DownloadString("https://playepikservercontents.netlify.app/dependencies/dependencies.json");
+                JObject data = JObject.Parse(json);
+
+                string currentVersion = Application.ProductVersion;
+                string latestVersion = data["versionDependenciesInstaller"]?.ToString();
+
+                //CREATE VERION OBJECTS
+                Version latest = new Version(latestVersion);
+                Version current = new Version(currentVersion);
+
+                int result = current.CompareTo(latest);
+
+                if (result < 0)
+                {
+                    var message = MessageBox.Show("A new update is available! (v. " + latestVersion + "), do you want to update it?", "Updater", MessageBoxButtons.YesNo);
+                    if(message == DialogResult.Yes)
+                    {
+                        ProcessStartInfo website = new ProcessStartInfo("https://github.com/Andrea-Filice/DependenciesInstaller/releases/latest");
+                        Process.Start(website);
+                    }
+                }
+                else
+                    MessageBox.Show("You have the latest version available.", "Updater", MessageBoxButtons.OK);
+            }
+        }
     }
 }
