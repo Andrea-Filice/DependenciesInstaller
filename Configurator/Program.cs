71fadcb145e457482e563e67bbe817f71ad96650
Andrea-Filice <filiceandrea8@gmail.com>
2025-11-08 00:17:19 +0100

    fix: fixed return type
    * fixed the return of the current hour (suggested by codeql)

--- Configurator/Program.cs
+++ Configurator/Program.cs
@@ -243,10 +243,7 @@ namespace Configurator
         }
         public static string GetCurrentDate()
         {
-            if(DateTime.Now.Second < 10)
-                return $"{DateTime.Now.Hour}:{DateTime.Now.Minute}:0{DateTime.Now.Second}";
-            else
-                return $"{DateTime.Now.Hour}:{DateTime.Now.Minute}:{DateTime.Now.Second}";
+            return $"{(DateTime.Now.Hour < 10 ? "0" : "")}{DateTime.Now.Hour}:{(DateTime.Now.Minute < 10 ? "0" : "")}{DateTime.Now.Minute}:{(DateTime.Now.Second < 10 ? "0" : "")}{DateTime.Now.Second}";
         }
     }
 }
