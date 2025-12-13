5dd75ab9fb39dbd953bfc1d4adea8929b9cd18bf
Andrea-Filice <filiceandrea8@gmail.com>
2025-12-13 15:32:51 +0100

    soc: codeql alert fix

--- Configurator/forms/AboutDialog.Designer.cs
+++ Configurator/forms/AboutDialog.Designer.cs
@@ -99,7 +99,7 @@ namespace Configurator.forms
         private System.Windows.Forms.Label appName;
         private System.Windows.Forms.Label appVersion;
         private System.Windows.Forms.Button buttonOK;
-        private string buildNumber = "13122025C";
+        private readonly string buildNumber = "13122025C";
 
         private void OnLoad() {appVersion.Text = $"{Application.ProductVersion} ({buildNumber})";}
     }
