1dc91eb657e00b88fb3c486b303fa9155e8e2cef
Andrea-Filice <filiceandrea8@gmail.com>
2025-12-13 13:45:52 +0100

    chore: preparing next release
    * preparing for the next release

--- Configurator/forms/AboutDialog.Designer.cs
+++ Configurator/forms/AboutDialog.Designer.cs
@@ -55,9 +55,9 @@
             this.appVersion.Font = new System.Drawing.Font("Manrope", 10.8F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.appVersion.Location = new System.Drawing.Point(85, 76);
             this.appVersion.Name = "appVersion";
-            this.appVersion.Size = new System.Drawing.Size(166, 25);
+            this.appVersion.Size = new System.Drawing.Size(169, 25);
             this.appVersion.TabIndex = 2;
-            this.appVersion.Text = "2.1.0.1 (07122025B)";
+            this.appVersion.Text = "2.1.0.2 (13122025C)";
             // 
             // buttonOK
             // 
