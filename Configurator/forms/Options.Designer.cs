450220b3920efb91c69175584f7b704e91d5cc6c
Andrea-Filice <filiceandrea8@gmail.com>
2025-12-13 14:22:54 +0100

    feat: adding infos
    * added more infos in Options tab

--- Configurator/forms/Options.Designer.cs
+++ Configurator/forms/Options.Designer.cs
@@ -24,13 +24,17 @@ namespace Configurator
             this.onBuild = new System.Windows.Forms.CheckBox();
             this.eacVersions = new System.Windows.Forms.ComboBox();
             this.versionText = new System.Windows.Forms.Label();
+            this.infoOpt1 = new System.Windows.Forms.PictureBox();
+            this.infoOpt2 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.infoOpt1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.infoOpt2)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(70, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(82, 12);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(40, 36);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
@@ -51,7 +55,7 @@ namespace Configurator
             // 
             this.selectOnStart.AutoSize = true;
             this.selectOnStart.Font = new System.Drawing.Font("Clash Display Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.selectOnStart.Location = new System.Drawing.Point(24, 74);
+            this.selectOnStart.Location = new System.Drawing.Point(14, 70);
             this.selectOnStart.Name = "selectOnStart";
             this.selectOnStart.Size = new System.Drawing.Size(337, 27);
             this.selectOnStart.TabIndex = 2;
@@ -62,7 +66,7 @@ namespace Configurator
             // 
             this.onBuild.AutoSize = true;
             this.onBuild.Font = new System.Drawing.Font("Clash Display Medium", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.onBuild.Location = new System.Drawing.Point(23, 113);
+            this.onBuild.Location = new System.Drawing.Point(14, 114);
             this.onBuild.Name = "onBuild";
             this.onBuild.Size = new System.Drawing.Size(285, 27);
             this.onBuild.TabIndex = 3;
@@ -92,11 +96,35 @@ namespace Configurator
             this.versionText.TabIndex = 5;
             this.versionText.Text = "EAC Version:";
             // 
+            // infoOpt1
+            // 
+            this.infoOpt1.Image = ((System.Drawing.Image)(resources.GetObject("infoOpt1.Image")));
+            this.infoOpt1.Location = new System.Drawing.Point(348, 73);
+            this.infoOpt1.Name = "infoOpt1";
+            this.infoOpt1.Size = new System.Drawing.Size(18, 17);
+            this.infoOpt1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.infoOpt1.TabIndex = 6;
+            this.infoOpt1.TabStop = false;
+            this.infoOpt1.Click += new System.EventHandler(this.infoOpt1_Click);
+            // 
+            // infoOpt2
+            // 
+            this.infoOpt2.Image = ((System.Drawing.Image)(resources.GetObject("infoOpt2.Image")));
+            this.infoOpt2.Location = new System.Drawing.Point(297, 119);
+            this.infoOpt2.Name = "infoOpt2";
+            this.infoOpt2.Size = new System.Drawing.Size(18, 17);
+            this.infoOpt2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.infoOpt2.TabIndex = 7;
+            this.infoOpt2.TabStop = false;
+            this.infoOpt2.Click += new System.EventHandler(this.infoOpt2_Click);
+            // 
             // Options
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(382, 253);
+            this.Controls.Add(this.infoOpt2);
+            this.Controls.Add(this.infoOpt1);
             this.Controls.Add(this.versionText);
             this.Controls.Add(this.eacVersions);
             this.Controls.Add(this.onBuild);
@@ -111,6 +139,8 @@ namespace Configurator
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Options_FormClosed);
             this.Load += new System.EventHandler(this.Options_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.infoOpt1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.infoOpt2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -124,5 +154,7 @@ namespace Configurator
         public System.Windows.Forms.CheckBox onBuild;
         public System.Windows.Forms.ComboBox eacVersions;
         private System.Windows.Forms.Label versionText;
+        private System.Windows.Forms.PictureBox infoOpt1;
+        private System.Windows.Forms.PictureBox infoOpt2;
     }
 }
