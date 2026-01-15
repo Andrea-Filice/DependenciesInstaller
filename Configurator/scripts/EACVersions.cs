12ecd8eed0eff05968e18e8fdca915139a027057
Andrea Filice <filiceandrea8@gmail.com>
2026-01-15 17:20:23 +0100

    fix: fixing EAC versions

--- Configurator/scripts/EACVersions.cs
+++ Configurator/scripts/EACVersions.cs
@@ -2,6 +2,6 @@
 {
     public class EACVersions
     {
-        public static string[] compatibleVersions = { "c935ac1c65ef14", "da4a22a2247c38" };
+        public static string[] compatibleVersions = { "be2efb9dd6ee25", "c935ac1c65ef14" };
     }
 }
