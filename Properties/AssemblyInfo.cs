eb6bc810d7d9b907d878c99bb1b86397c29a2039
Andrea Filice <filiceandrea8@gmail.com>
2026-01-15 17:07:24 +0100

    chore: started new release

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("bef407e2-6cda-416b-a7e1-10b20ed814ba")]
 
-[assembly: AssemblyVersion("2.1.1.2")]
-[assembly: AssemblyFileVersion("2.1.1.2")]
+[assembly: AssemblyVersion("2.1.1.3")]
+[assembly: AssemblyFileVersion("2.1.1.3")]
