f9b0b340fe4ba9b97474939ef907f728b5f2bf20
Andrea Filice <filiceandrea8@gmail.com>
2026-02-10 21:51:00 +0100

    chore: bumped EAC
    * bumped Easy Anti Cheat

--- Configurator/scripts/EACVersions.cs
+++ Configurator/scripts/EACVersions.cs
@@ -2,6 +2,6 @@
 {
     public class EACVersions
     {
-        public static string[] compatibleVersions = { "v2601.1", "v2512.0" };
+        public static string[] compatibleVersions = { "v2601.2", "v2601.1" };
     }
 }
