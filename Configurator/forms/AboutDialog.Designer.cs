84309105b33b1702fac689a100f55be42e6d8bb0
Andrea Filice <filiceandrea8@gmail.com>
2026-09-10 11:04:06 +0200

    chore: bumped versions
    * started a new release cycle

--- Configurator/forms/AboutDialog.Designer.cs
+++ Configurator/forms/AboutDialog.Designer.cs
@@ -100,7 +100,7 @@ namespace Configurator.forms
         private System.Windows.Forms.Label appName;
         private System.Windows.Forms.Label appVersion;
         private System.Windows.Forms.Button buttonOK;
-        private readonly string buildNumber = "14082026M";
+        private readonly string buildNumber = "10092026N";
 
         private void OnLoad() {appVersion.Text = $"{Application.ProductVersion} ({buildNumber})";}
         private ToolTip toolTipAbout;
